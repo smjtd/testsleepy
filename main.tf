e9099597e79973a387e46ae83a2a4c02a3cde116
Soumyajit Das <55692060+smjtd@users.noreply.github.com>
2021-12-16 10:35:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,21 @@ resource "null_resource" "local-exec" {
     command = "sleep 2"
   }
 }
-resource "null_resource" "local-exec1" {
-  provisioner "local-exec" {
-    command = "sleep 5"
-  }
-}
-resource "null_resource" "local-exec2" {
-  provisioner "local-exec" {
-    command = "sleep 10"
-  }
-}
-resource "null_resource" "local-exe3" {
-  provisioner "local-exec" {
-    command = "sleep 15"
-  }
-}
+# resource "null_resource" "local-exec1" {
+#   provisioner "local-exec" {
+#     command = "sleep 5"
+#   }
+# }
+# resource "null_resource" "local-exec2" {
+#   provisioner "local-exec" {
+#     command = "sleep 10"
+#   }
+# }
+# resource "null_resource" "local-exe3" {
+#   provisioner "local-exec" {
+#     command = "sleep 15"
+#   }
+# }
 resource "null_resource" "local-exe4" {
   provisioner "local-exec" {
     command = "sleep 40"
